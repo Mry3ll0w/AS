bc47e02856d90b066752f3b4468e3a1b5cb152bf
ara <aradxc56@gmail.com>
2022-03-23 21:59:31 +0100

    Vagrant file settings para p2

--- vagrant_files/provision_file.sh
+++ vagrant_files/provision_file.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-
 apt-get update
-apt-get upgrade -y
-apt-get install -y apache2
-#if ! [ -L /var/www ]; then
-#  rm -rf /var/www
-#  ln -fs /vagrant /var/www
-#fi
+apt-get -y upgrade
+apt-get install -y nmap 
+apt-get install -y iptables
 
