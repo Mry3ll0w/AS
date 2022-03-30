ed5a1af3cb33c979a98cd78a74154ccd2aea6670
ara <aradxc56@gmail.com>
2022-03-30 17:58:09 +0200

    Ejercicios 1 y 2

--- vagrant_files/provision_file.sh
+++ vagrant_files/provision_file.sh
@@ -3,4 +3,6 @@ apt-get update
 apt-get -y upgrade
 apt-get install -y nmap 
 apt-get install -y iptables
+#Agregamos al /etc/hosts para obtener las ips
+sudo cat /vagrant/ip_hosts.txt >> /etc/hosts
 
