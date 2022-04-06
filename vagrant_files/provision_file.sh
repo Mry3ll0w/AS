df4b5b92fe5b7c13b92fd8a455a3ebcc2290e7f3
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-04-06 17:48:03 +0200

    :poop:Ejercicio 3

--- vagrant_files/provision_file.sh
+++ vagrant_files/provision_file.sh
@@ -5,3 +5,8 @@ apt-get install -y iptables
 #Agregamos al /etc/hosts para obtener las ips
 sudo cat /vagrant/ip_hosts.txt >> /etc/hosts
 
+#Para iptables (requerido a partir del apartado 3)
+
+sudo iptables -A INPUT -s localhost -j ACCEPT
+sudo iptables -A INPUT -j ACCEPT -m conntrack --ctstate	ESTABLISHED
+sudo iptables	-P INPUT DROP
