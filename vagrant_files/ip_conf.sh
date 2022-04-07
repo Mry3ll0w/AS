3fcf038ad36c0877bee523fd0df764e4bd4531a8
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-04-07 10:38:32 +0200

    cositas vagrant

---
+++ vagrant_files/ip_conf.sh
@@ -0,0 +1,3 @@
+-sudo iptables -A INPUT -s localhost -j ACCEPT
+sudo iptables -A INPUT -j ACCEPT -m conntrack --ctstate	ESTABLISHED
+sudo iptables	-P INPUT DROP
--- vagrant_files/provision_file.sh
+++ vagrant_files/provision_file.sh
@@ -6,7 +6,3 @@ apt-get install -y iptables
 sudo cat /vagrant/ip_hosts.txt >> /etc/hosts
 
 #Para iptables (requerido a partir del apartado 3)
-
-sudo iptables -A INPUT -s localhost -j ACCEPT
-sudo iptables -A INPUT -j ACCEPT -m conntrack --ctstate	ESTABLISHED
-sudo iptables	-P INPUT DROP
